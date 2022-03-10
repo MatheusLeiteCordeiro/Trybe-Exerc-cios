d0d786b148da3898942f2982c89700cd9a170490
Matheus Emanuel <matheusleitecordeiro@gmail.com>
2022-03-10 20:30:16 -0300

    a

--- Fundamentos/bloco-04-Introducao-a-JavaScript-e-Logica-de-Programacao/dia-03-JavaScript - Logica-de-Programacao-e-Algoritmos/exercicios.js
+++ Fundamentos/bloco-04-Introducao-a-JavaScript-e-Logica-de-Programacao/dia-03-JavaScript - Logica-de-Programacao-e-Algoritmos/exercicios.js
@@ -9,13 +9,13 @@
 
 // console.log(x);
 
-// let word = 'subinoonibus';
-// let result = '';
-// for(i=word.length - 1; i>=0; i--) {
-//     result += word[i]
-// }
+let word = 'subinoonibus';
+let result = '';
+for(i=word.length; i>=0; i--) {
+    result += word[i]
+}
 
-// console.log(result);
+console.log(result);
 
 // 3.1
 
@@ -44,10 +44,10 @@
 
 // 4
 
-let maior = 0
-for(let i=0; i<=50; i++){
-    for(let o=2; o<=50; o++)
-    if(i % o === 0 && i % i === 0 && i % 1 === 0 && i > maior) {
-        console.log(i)
-    }
-}
+// let maior = 0
+// for(let i=0; i<=50; i++){
+//     for(let o=2; o<=50; o++)
+//     if(i % o === 0 && i % i === 0 && i % 1 === 0 && i > maior) {
+//         console.log(i)
+//     }
+// }
