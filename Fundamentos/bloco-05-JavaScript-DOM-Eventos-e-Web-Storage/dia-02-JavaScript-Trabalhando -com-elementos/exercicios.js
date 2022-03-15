35f758515101e728cb584260dc3f88d67b124547
Matheus Emanuel <matheusleitecordeiro@gmail.com>
2022-03-15 20:31:33 -0300

    exercicios 7, 8, 9, 10, 11, 12, 13  check

--- Fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia-02-JavaScript-Trabalhando -com-elementos/exercicios.js
+++ Fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia-02-JavaScript-Trabalhando -com-elementos/exercicios.js
@@ -30,4 +30,46 @@ let newSection2 = document.createElement('section2');
 newSection2.className = 'rigth-content';
 newMain.appendChild(newSection2);
 
+// 7.
+let newImg = document.createElement('img');
+newImg.className = 'small-image';
+newImg.src = 'https://picsum.photos/200'
+newSection1.appendChild(newImg);
+
+// 8.
+let lista = ['um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez'];
+let newlU = document.createElement('ul');
+for (let i=0; i<lista.length; i++) {
+   let newLi = document.createElement('li');
+   newLi.innerText = lista[i];
+   newlU.appendChild(newLi);
+};
+newSection2.appendChild(newlU);
+
+// 9.
+let newH3_1 = document.createElement('h3'); 
+let newH3_2 = document.createElement('h3'); 
+let newH3_3 = document.createElement('h3'); 
+newMain.appendChild(newH3_1);
+newMain.appendChild(newH3_2);
+newMain.appendChild(newH3_3);
+
+// 10.
+newH1.className = 'title';
+
+// 11.
+newH3_1.className = 'description';
+newH3_2.className = 'description';
+newH3_3.className = 'description';
+
+// 12.
+newMain.removeChild(newSection1);
+
+// 13.
+newSection2.style.marginLeft = '50%';
+newSection2.innerText = 'centro'
+
 console.log(container);
+
+
+
