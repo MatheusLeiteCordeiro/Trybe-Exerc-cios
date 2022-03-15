3e4314b98626984e0308c89f999d4c07c7445b39
Matheus Emanuel <matheusleitecordeiro@gmail.com>
2022-03-15 19:08:24 -0300

    exercicios 2 e 3 check

--- Fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia-02-JavaScript-Trabalhando -com-elementos/exercicios.js
+++ Fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia-02-JavaScript-Trabalhando -com-elementos/exercicios.js
@@ -1,5 +1,20 @@
+// 1.
 let container = document.getElementById('body');
 let newH1 = document.createElement('h1');
 container.appendChild(newH1);
-      
+newH1.className = 'h1';
+let h1 = document.getElementById('h1');
+newH1.innerText = ' Exercício 5.2 - JavaScript DOM';
+
+// 2.
+let newMain = document.createElement('main');
+newMain.className = 'main-content';
+container.appendChild(newMain);
+
+// 3.
+let newSection = document.createElement('section');
+newSection.className = 'center-content';
+newMain.appendChild(newSection);
+
 console.log(container);
+
