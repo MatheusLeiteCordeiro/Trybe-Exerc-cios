6a41903e67f6168ab67d21d98ef01196a79dbed0
Matheus Emanuel <matheusleitecordeiro@gmail.com>
2022-03-28 23:40:39 -0300

    exercicios 5.1

--- Fundamentos/bloco-05-JavaScript:DOM-Eventos-e-Web-Storage/scripts.js
+++ Fundamentos/bloco-05-JavaScript:DOM-Eventos-e-Web-Storage/scripts.js
@@ -1,1 +1,27 @@
-sdsfsdfsdf
+let h = document.querySelector('#header-container');
+
+h.style.backgroundColor = 'green';
+
+let u_i = document.querySelectorAll(".emergency-tasks h3");
+
+for (let i=0; i<u_i.length; i++) {
+
+    u_i[i].style.backgroundColor = 'blue';
+}
+
+let n_u_i = document.querySelectorAll(".no-emergency-tasks h3");
+
+for (let i=0; i<n_u_i.length; i++) {
+
+    n_u_i[i].style.backgroundColor = 'black';
+}
+
+let f = document.getElementsByTagName('footer')[0];
+f.style.backgroundColor = 'black';
+
+let s1 = document.querySelectorAll('section')[0];
+s1.style.backgroundColor = 'red'
+
+let s2 = document.querySelectorAll('section')[1];
+s2.style.backgroundColor = 'yellow'
+
