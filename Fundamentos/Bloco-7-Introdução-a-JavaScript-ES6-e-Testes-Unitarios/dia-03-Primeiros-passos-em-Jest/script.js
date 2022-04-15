02b1b494451f038b5ebf1fd54e98b88eb0c16318
Matheus Emanuel <matheusleitecordeiro@gmail.com>
2022-04-14 21:22:40 -0300

    exercicios/7.3

---
+++ Fundamentos/Bloco-7-Introdução-a-JavaScript-ES6-e-Testes-Unitarios/dia-03-Primeiros-passos-em-Jest/script.js
@@ -0,0 +1,5 @@
+const assert = require('assert'); // Sintaxe para incluir o módulo assert
+
+// assert.strictEqual(50, 50); // Sem erros: 50 === 50
+
+assert.strictEqual(0, true); // AssertionError: 50 !== 70
