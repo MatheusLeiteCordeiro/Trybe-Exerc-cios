623d46981a4c5a5278f9d177e6e556a50c49b95d
Matheus Emanuel <matheusleitecordeiro@gmail.com>
2022-08-19 11:36:56 -0300

    dia 12.1

--- Desenvolvimento-Front-End/Bloco12- Ciclo-de-Vida-de-Componentes-e-React-Router/dia-01-Ciclo-de-vida-de-componentes/my_app/src/App.js
+++ Desenvolvimento-Front-End/Bloco12- Ciclo-de-Vida-de-Componentes-e-React-Router/dia-01-Ciclo-de-vida-de-componentes/my_app/src/App.js
@@ -35,4 +35,48 @@ class App extends Component {
   }
 }
 
+// class App extends Component {
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       counter: 0
+//     };
+
+//     console.log("construtor");
+//   }
+
+//   componentDidMount() {
+//     console.log("componentDidMount");
+//   }
+
+//   shouldComponentUpdate(nextProps, nextState) {
+//     console.log("shouldComponentUpdate", this.state, nextState);
+//     if (nextState.counter === 5) {
+//       this.setState({ counter: 0 })
+//     }
+
+//     return true;
+//   }
+
+//   componentDidUpdate(prevProps, prevState) {
+//     console.log("componentDidUpdate", this.state, prevState);
+//   }
+
+//   render() {
+//     console.log("render");
+//     return (
+//       <div>
+//         <p>Contador</p>
+//         <button
+//           onClick={() => this.setState((state) => ({ counter: state.counter + 1 }))}
+//         >
+//           Soma
+//         </button>
+//         <p>{this.state.counter}</p>
+//       </div>
+//     );
+//   }
+// }
+
 export default App;
